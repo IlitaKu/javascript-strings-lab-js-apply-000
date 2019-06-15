f0c416ef20d070eed0beb0e8c8e9f75c6cc04462
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-06-15 15:28:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,79 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 var topic = "space";
 
 var conversation = "Let's talk about " + `${topic}` + ".";
+
+function sayHello(){
+  console.log("Hello, there!")
+}
+sayHello()
+function sayHi(){
+  console.log("Hello, Viktor!")
+}
+sayHi()
+function greet(){
+  console.log("Hello, Ilita!")
+}
+greet()
+function doSomething(anything){
+  console.log(anything)
+}
+doSomething('Morning')
+
+function burger(meat){
+  console.log(`This is a ${meat} hamburger`)
+}
+burger("chicken")
+
+function firstLast(name, surname){
+  console.log(`${name} ${surname}`)
+}
+firstLast("Ilita", "Kunelyte")
+
+function divide(x, y) {
+  return x + y
+}
+
+function add(animal, category){
+  console.log("What is this?")
+  return `${animal} beltongs to ${category}.`
+}
+add("Cat", "Mammals")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
